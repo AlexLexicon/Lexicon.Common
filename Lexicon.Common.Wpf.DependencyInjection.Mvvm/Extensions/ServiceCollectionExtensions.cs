b9682593f650b9c237fd4a18215939311bfa78b2
Alex Stroot <alexrs.business@outlook.com>
2023-01-24 00:33:28 -0800

    added DataContextFactory to di when DataContexts are added

--- Lexicon.Common.Wpf.DependencyInjection.Mvvm/Extensions/ServiceCollectionExtensions.cs
+++ Lexicon.Common.Wpf.DependencyInjection.Mvvm/Extensions/ServiceCollectionExtensions.cs
@@ -2,6 +2,7 @@
 using Lexicon.Common.Wpf.DependencyInjection.Mvvm.Builders;
 using Lexicon.Common.Wpf.DependencyInjection.Mvvm.Factories;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Lexicon.Common.Wpf.DependencyInjection.Mvvm.Extensions;
 public static class ServiceCollectionExtensions
@@ -9,6 +10,9 @@ public static class ServiceCollectionExtensions
     public static AddDataContextBuilder<TDataContext> AddDataContext<TDataContext>(this IServiceCollection services) where TDataContext : class
     {
         ArgumentNullException.ThrowIfNull(services);
+
+        //we want to add this factory if any data contexts are added
+        services.TryAddSingleton<IDataContextFactory, DataContextFactory>();
 
         //add the DataContext
         services.AddTransient<TDataContext>();
