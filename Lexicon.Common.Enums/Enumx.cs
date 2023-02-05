d640496e13efbcfc0a594a364fde9448b65887b2
Alex Stroot <alexrs.business@outlook.com>
2023-02-05 14:38:59 -0800

    added Lexicon.Common.Enums

---
+++ Lexicon.Common.Enums/Enumx.cs
@@ -0,0 +1,15 @@
+﻿using Lexicon.Common.Enums.Extensions;
+
+namespace Lexicon.Common.Enums;
+public static class Enumx
+{
+    public static string? GetDescription(Enum value)
+    {
+        return value.GetDescription();
+    }
+
+    public static IEnumerable<T> GetValues<T>() where T : struct, Enum
+    {
+        return Enum.GetValues<T>();
+    }
+}
---
+++ Lexicon.Common.Enums/Extensions/EnumExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace Lexicon.Common.Enums.Extensions;
+public static class EnumExtensions
+{
+    public static string? GetDescription(this Enum value)
+    {
+        FieldInfo? field = value.GetType().GetField(value.ToString());
+
+        if (field is null)
+        {
+            return null;
+        }
+
+        DescriptionAttribute[]? attributes = field.GetCustomAttributes(typeof(DescriptionAttribute), false) as DescriptionAttribute[];
+
+        DescriptionAttribute? descriptionAttribute = attributes?.FirstOrDefault();
+
+        if (descriptionAttribute is not null)
+        {
+            return descriptionAttribute.Description;
+        }
+
+        return value.ToString();
+    }
+}
