88d0cbc6190a910cf58ff1b10dcf83c7eaf5f1fb
Alex Stroot <alexrs.business@outlook.com>
2023-02-05 15:48:56 -0800

    added the ButtonForegroundImage control which can be used as a button's context to add an image that binds to the foreground as the opactity mask. This simplifies creating icon based buttons

---
+++ Lexicon.Common.Wpf.Controls/ButtonForegroundImage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Lexicon.Common.Wpf.Controls;
+public partial class ButtonForegroundImage : Label
+{
+    public ButtonForegroundImage() => InitializeComponent();
+
+    public static readonly DependencyProperty ImageSourceProperty = DependencyProperty.Register(nameof(ImageSource), typeof(string), typeof(ButtonForegroundImage));
+    public string? ImageSource
+    {
+        get => (string?)GetValue(ImageSourceProperty);
+        set => SetValue(ImageSourceProperty, value);
+    }
+}
